82a9e2d42ee2a79fea8c110e64704cc94ada2257
AWolf81 <awolf2904@gmail.com>
2018-03-28 10:33:31 +0200

    added user transformer

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Transformers;
+
+use App\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+
+    /**
+     * Turn this item object into a generic array
+     *
+     * @return array
+     */
+    public function transform(User $user)
+    {
+        return [
+            'id'            => (int) $user->id,
+            'name'          => $user->name,
+            'email'         => $user->email
+        ];
+    }
+
+}
